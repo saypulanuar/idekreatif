251b18b21514e75fc4024fac5fa458582d346726
saypulanuar <saypulanuar53@gmail.com>
2025-01-14 15:33:42 +0700

    tambah tugas lagi

--- auth/register_process.php
+++ auth/register_process.php
@@ -1,29 +1,32 @@
 <?php
 require_once("../config.php");
+// Mulai session
 session_start();
 
-if ($_SERVER["REQUEST_METHOD"] == "post"){
-    $username = $_POST ["username"];
-    $name = $_POST ["name"];
-    $password = $_POST ["password"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $name = $_POST["name"];
+    $password = $_POST["password"];
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    $Sql = "INSERT INTO users (username, name, password)
-    VALUES ('$username', '$name', '$$hashedPassword')";
-    if ($conn->query($sql)=== TRUE){
-        $_SESSION['notification']=[
+    $sql = "INSERT INTO users (username, name, password) 
+            VALUES ('$username', '$name', '$hashedPassword')";
+    
+    if ($conn->query($sql) === TRUE) {
+        // Simpan notifikasi ke dalam session
+        $_SESSION['notification'] = [
             'type' => 'primary',
             'message' => 'Registrasi Berhasil!'
         ];
-    } else{
+    } else {
         $_SESSION['notification'] = [
             'type' => 'danger',
-            'message' => 'Gagal Regristrasi: '.mysqli_error($conn) 
+            'message' => 'Gagal Registrasi: ' . mysqli_error($conn)
         ];
-
     }
     header('Location: login.php');
     exit();
 }
+
 $conn->close();
 ?>
