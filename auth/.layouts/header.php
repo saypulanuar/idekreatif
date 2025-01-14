8acac1d2069560d96b684240543366b134ad41c1
saypulanuar <saypulanuar53@gmail.com>
2025-01-14 14:31:32 +0700

    tambah 4 tugas

--- auth/.layouts/header.php
+++ auth/.layouts/header.php
@@ -1,3 +1,10 @@
+<?php
+session_start(); 
+$notification = $_SESSION['notification'] ?? null;
+if ($notification){
+  unset($_SESSION['notification']);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
